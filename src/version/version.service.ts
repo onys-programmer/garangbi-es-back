33ae3d9938ab0bc8bb223ffb2cdfca424509e8c0
taehokim-onys <thkim2017@gmail.com>
2023-08-06 23:44:36 +0900

    feat: 🎸 version changes add

--- src/version/version.service.ts
+++ src/version/version.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class VersionService {
-  getLatestVersion(): { version: string } {
-    return { version: '1.0.1' };
+  getLatestVersion(): { version: string; changes: string[] } {
+    return {
+      version: '1.0.1',
+      changes: [
+        '띄어쓰기, 기호 틀려도 정답 처리되게',
+        '문제 순서 랜덤으로 변경',
+      ],
+    };
   }
 }
