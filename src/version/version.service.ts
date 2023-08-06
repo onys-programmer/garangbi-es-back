22c5e203697accf00e2c29565c65d867409b6f11
taehokim-onys <thkim2017@gmail.com>
2023-08-06 19:36:33 +0900

    feat: 🎸 update to 1.0.1

--- src/version/version.service.ts
+++ src/version/version.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class VersionService {
   getLatestVersion(): { version: string } {
-    return { version: '1.0.0' };
+    return { version: '1.0.1' };
   }
 }
