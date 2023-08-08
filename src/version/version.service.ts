a68c9d25134088186af19bdb6c3aaae574aa9ae5
taehokim-onys <thkim2017@gmail.com>
2023-08-08 20:23:34 +0900

    feat: 🎸 ver to 1.0.2

--- src/version/version.service.ts
+++ src/version/version.service.ts
@@ -5,7 +5,7 @@ import { ApiResponse } from 'src/utils/apiResponse';
 export class VersionService {
   getLatestVersion(): ApiResponse<{ version: string; changes: string[] }> {
     return new ApiResponse({
-      version: '1.0.1',
+      version: '1.0.2',
       changes: ['띄어쓰기, 기호 채점 기준에서 제외', '문제 순서 랜덤으로 변경'],
     });
   }
